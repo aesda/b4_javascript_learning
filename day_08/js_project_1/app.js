4d7cd53ca8bf6228803178b6a108158e06e946da
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-12-10 21:02:21 +1100

    added new js

---
+++ day_08/js_project_1/app.js
@@ -0,0 +1,24 @@
+let counter = 10;
+const plusBtn = document.getElementById('plus_btn');
+plusBtn.addEventListener('click', function() {
+    counter = counter + getStep;
+    writeCounter();
+
+});
+
+const minusBtn = document.getElementById('minus_btn');
+minusBtn.addEventListener('click', function() {
+    counter = counter - getStep;
+    writeCounter();
+});
+
+function writeCounter() {
+   const label = document.querySelector('.label');
+   label.innerHTML = counter;
+}
+
+function getStep() {
+    const step = document.querySelector('input');
+    const count = step.value || 1;
+    return parseInt(count);
+}
---
+++ day_08/js_project_2/app.js
@@ -0,0 +1,36 @@
+
+
+const buttons = document.querySelectorAll('button');
+
+let actions = [];
+let previousValue;
+
+
+for(let button of buttons) {
+    button.addEventListener('click', function(event)
+{
+    const value = event.target.textContent;
+    if(value === '=') {
+        const expression = actions.join('');
+        const result = eval(expression);
+        actions = [result];
+        
+
+    }
+    else {
+        if(previousValue === "=") {
+            actions = [value];
+
+        }
+        else {
+        actions.push(value); }
+
+    }
+    previousValue = value;
+    
+    const screen = document.getElementById('screen');
+    screen.textContent = actions.join('');
+    
+})
+}
+//operators ar epressed multiple tumes consecutively.
